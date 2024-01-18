d6d0e9f9b5cb67a11162e01a4a893a3003e07140
Stavin Alban <stavinalban@gmail.com>
2024-01-18 20:56:31 +0530

    Feathure Added:Calcualting Feathure

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
