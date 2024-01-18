cb0ce14846f25af76df6a32267c748f83fce2d3a
Stavin Alban <stavinalban@gmail.com>
2024-01-18 21:07:21 +0530

    Feathure Added: Calculating Daily Wage of a Employee

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int FULL_TIME = 1;
+        int emphr=0,empwage=0;
+        int wageperhr=20;
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
         if (empCheck == FULL_TIME) {
-            System.out.println("Employee is Present");
+            //System.out.println("Employee is Present");
+            emphr=8;
         } else {
             System.out.println("Employee is Absent");
         }
+        empwage=emphr*wageperhr;
+        System.out.println("Emplyee wage for day ="+empwage);
     }
 }
